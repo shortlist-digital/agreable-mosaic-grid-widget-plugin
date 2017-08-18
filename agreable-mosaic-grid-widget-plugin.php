8be95dc69b5b08a194d759201951f6c429d4e712
Leo <leonoragilmour@sky.com>
2017-08-18 14:58:45 +0100

    init

---
+++ agreable-mosaic-grid-widget-plugin.php
@@ -0,0 +1,22 @@
+<?php
+/**
+* @wordpress-plugin
+* Plugin Name: Agreable Mosaic Grid Widget Plugin
+* Plugin URI: http://github.com/shortlist-digital/agreable-mosaic-grid-widget-plugin
+* Description: A plugin for the mosaic grid widget
+* Version: 1.0.0
+* Author: Shortlist Studio
+* Author URI: http://shortlist.studio
+* License: MIT
+*/
+
+require_once __DIR__ . '/../../../../vendor/autoload.php';
+
+class AgreableMosaicGridWidgetPlugin
+{
+    public function __construct()
+    {
+        //
+    }
+}
+new AgreableMosaicGridWidgetPlugin();
---
+++ readme.md
@@ -0,0 +1,2 @@
+# Agreable Mosaic Grid Widget Plugin
+A plugin for the mosaic grid widget
